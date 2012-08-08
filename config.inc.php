9d945471aa27230686598682e5e1453f33cfc2f5
AlloVince <allo.vince@gmail.com>
2012-08-08 15:33:52 +0800

    add config.inc

---
+++ config.inc.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+    'libPath' => __DIR__ . '/lib',
+    'sourceRootPath' => __DIR__ . '/upload',
+    'thumbFileRootPath' => __DIR__ . '/thumb',
+    'thumbUrlRootPath' => __DIR__ . '/..' ,
+    'saveImage' => false,
+);
