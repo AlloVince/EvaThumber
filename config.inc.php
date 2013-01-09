394c6e9335a480e39346e239be64ef69d6ecba20
AlloVince <allo.vince@gmail.com>
2013-01-09 17:50:15 +0800

    config changes

--- config.inc.php
+++ config.inc.php
@@ -2,6 +2,7 @@
 return array(
     //'debug' => false,
     //'error_redirect' => 'http://avnpc.com/pages/evacloudimage',
+    'classPath' => __DIR__ . '/EvaCloudImage.php',  //EvaCloudImage class file path
     'libPath' => __DIR__ . '/lib',  //PHPthumb library path
     'sourceRootPath' => __DIR__ . '/upload',  //original image save path, require path read permission
     'thumbFileRootPath' => __DIR__ . '/thumb', //resized thumbnails save path, require read and write permission
--- index.php
+++ index.php
@@ -9,7 +9,6 @@
  * @author    AlloVince
  */
 
-require_once 'EvaCloudImage.php';
 error_reporting(E_ALL);
 
 // Check php version
@@ -18,5 +17,8 @@ if( version_compare(phpversion(), '5.3.0', '<') ) {
     exit(1);
 }
 
-$cloudImage = new EvaCloudImage(null, include 'config.inc.php');
+$config = include __DIR__ . 'config.inc.php';
+include $config['classPath'];
+
+$cloudImage = new EvaCloudImage(null, $config);
 $cloudImage->show();
