6ed547d1adb94e0b4b608ef641f6fac03b327037
AlloVince <allo.vince@gmail.com>
2013-04-16 17:01:33 +0800

    all blendings

--- src/EvaThumber/Imagine/Blend/AbstractBlend.php
+++ src/EvaThumber/Imagine/Blend/AbstractBlend.php
@@ -22,182 +22,277 @@ abstract class AbstractBlend
 {
     protected $resource;
 
-    protected static function pixelHandler($dstImage, $srcImage, $handler)
+    protected static function pixelHandler($topImage, $bottomImage, $handler)
     {
-        $width = $dstImage->getSize()->getWidth();
-        $height = $dstImage->getSize()->getHeight();
-        for ($x = 0; $x < $width; $x++) {
-            for ($y = 0; $y < $height; $y++) {
-                $point = new Point($x, $y);
-                $color = $dstImage->getColorAt($point);
-                $dR = $color->getRed();
-                $dG = $color->getGreen();
-                $dB = $color->getBlue();
-                $dA = $color->getAlpha();
-                $color = $srcImage->getColorAt($point);
-                $sR = $color->getRed();
-                $sG = $color->getGreen();
-                $sB = $color->getBlue();
-                $sA = $color->getAlpha();
-                $handler($point, $dR, $dG, $dB, $dA, $sR, $sG, $sB, $sA);
+        $width = $topImage->getSize()->getWidth();
+        $height = $topImage->getSize()->getHeight();
+
+        if($topImage === $bottomImage){
+            for ($x = 0; $x < $width; $x++) {
+                for ($y = 0; $y < $height; $y++) {
+                    $point = new Point($x, $y);
+                    $color = $topImage->getColorAt($point);
+                    $tR = $color->getRed();
+                    $tG = $color->getGreen();
+                    $tB = $color->getBlue();
+                    $tA = $color->getAlpha();
+                    $topImage->draw()->dot($point, new Color(array(
+                        $handler($tR, $bR),
+                        $handler($tG, $bG),
+                        $handler($tB, $bB)
+                    )));
+                }
+            }
+        } else {
+            for ($x = 0; $x < $width; $x++) {
+                for ($y = 0; $y < $height; $y++) {
+                    $point = new Point($x, $y);
+                    $color = $topImage->getColorAt($point);
+                    $tR = $color->getRed();
+                    $tG = $color->getGreen();
+                    $tB = $color->getBlue();
+                    $tA = $color->getAlpha();
+                    $color = $bottomImage->getColorAt($point);
+                    $bR = $color->getRed();
+                    $bG = $color->getGreen();
+                    $bB = $color->getBlue();
+                    $bA = $color->getAlpha();
+
+                    $topImage->draw()->dot($point, new Color(array(
+                        $handler($tR, $bR),
+                        $handler($tG, $bG),
+                        $handler($tB, $bB)
+                    )));
+                }
             }
         }
+
+
+
     }
 
-    public static function layerNormal($dstImage, $srcImage, $dstSource, $srcSource)
-    {
-        //普通
-    }
-
-    public static function layerDissolve($dstImage, $srcImage, $dstSource, $srcSource)
-    {
-        //溶解
-    }
-
-    public static function layerDarken($dstImage, $srcImage, $dstSource, $srcSource)
-    {
-        //变暗
-        self::pixelHandler($dstImage, $srcImage, function($point, $dR, $dG, $dB, $dA, $sR, $sG, $sB, $sA)
-            use ($dstImage){
-                $dstImage->draw()->dot($point, new Color(array(
-                    $dR > $sR ? $sR : $dR,
-                    $dG > $sG ? $sG : $dG,
-                    $dB > $sB ? $sB : $dB
-                )));
-        });
-    }
-
-    public static function layerMultiply($dstImage, $srcImage, $dstSource, $srcSource)
-    {
-        //正片叠底
-        self::pixelHandler($dstImage, $srcImage, function($point, $dR, $dG, $dB, $dA, $sR, $sG, $sB, $sA)
-            use ($dstImage){
-                $dstImage->draw()->dot($point, new Color(array(
-                    $dR * $sR / 255,
-                    $dG * $sG / 255,
-                    $dB * $sG / 255
-                )));
-        });
-    }
-
-    public static function layerColorBurn($dstImage, $srcImage, $dstSource, $srcSource)
-    {
-        //颜色加深
-    }
-
-    public static function layerLinearBurn($dstImage, $srcImage, $dstSource, $srcSource)
-    {
-        //线性加深
-        self::pixelHandler($dstImage, $srcImage, function($point, $dR, $dG, $dB, $dA, $sR, $sG, $sB, $sA)
-             use ($dstImage, $srcImage){
-                 $dstImage->draw()->dot($point, new Color(array(
-                     ($r = $dR + $sR) > 255 ? $r - 255 : 0,
-                     ($g = $dG + $sG) > 255 ? $g - 255 : 0,
-                     ($b = $dB + $sB) > 255 ? $b - 255 : 0
-                 )));
-        });
-    }
-
-    public static function layerLighten($dstImage, $srcImage, $dstSource, $srcSource)
+    public static function layerLighten($topImage, $bottomImage)
     {
         //变亮 
-        self::pixelHandler($dstImage, $srcImage, function($point, $dR, $dG, $dB, $dA, $sR, $sG, $sB, $sA)
-            use ($dstImage){
-                $dstImage->draw()->dot($point, new Color(array(
-                    $dR > $sR ? $dR : $sR,
-                    $dG > $sG ? $dG : $sG,
-                    $dB > $sB ? $dB : $sB
-                )));
+        //(B > A) ? B:A
+        self::pixelHandler($topImage, $bottomImage, function($A, $B) {
+            return $B > $A ? $B : $A;
         });
     }
 
 
-    public static function layerScreen($dstImage, $srcImage, $dstSource, $srcSource)
+    public static function layerDarken($topImage, $bottomImage)
     {
-        //滤色
-    }
-
-    public static function layerColorDodge($dstImage, $srcImage, $dstSource, $srcSource)
-    {
-        //颜色减淡
-        //TODO
-        self::pixelHandler($dstImage, $srcImage, function($point, $dR, $dG, $dB, $dA, $sR, $sG, $sB, $sA)
-            use ($dstImage){
-                $ssR = 255 - $sR == 0 ? 1 : 255 - $sR;
-                $ssG = 255 - $sG == 0 ? 1 : 255 - $sG;
-                $ssB = 255 - $sB == 0 ? 1 : 255 - $sB;
-                $dstImage->draw()->dot($point, new Color(array(
-                    $dR + $dR * $sR / (255 - $sR),
-                    $dG + $dG * $sG / (255 - $sG),
-                    $dB + $dB * $sB / (255 - $sB)
-                )));
+        //变暗
+        //(B > A) ? A:B
+        self::pixelHandler($topImage, $bottomImage, function($A, $B) {
+            return $B > $A ? $A : $B;
         });
     }
 
-    public static function layerLinearDodge($dstImage, $srcImage, $dstSource, $srcSource)
+    public static function layerMultiply($topImage, $bottomImage)
     {
-        //线性减淡
-        self::pixelHandler($dstImage, $srcImage, function($point, $dR, $dG, $dB, $dA, $sR, $sG, $sB, $sA)
-             use ($dstImage, $srcImage){
-                 $dstImage->draw()->dot($point, new Color(array(
-                     ($r = $dR + $sR) > 255 ? 255 : $r,
-                     ($g = $dG + $sG) > 255 ? 255 : $g,
-                     ($b = $dB + $sB) > 255 ? 255 : $b
-                 )));
+        //正片叠底
+        //(A * B) / 255
+        self::pixelHandler($topImage, $bottomImage, function($A, $B) {
+            return $A * $B / 255;
         });
     }
 
-    public static function layerOverlay($dstImage, $srcImage, $dstSource, $srcSource)
+    public static function layerAverage($topImage, $bottomImage)
     {
-        //叠加
-        self::pixelHandler($dstImage, $srcImage, function($point, $dR, $dG, $dB, $dA, $sR, $sG, $sB, $sA)
-             use ($dstImage, $srcImage){
-                $dstImage->draw()->dot($point, new Color(array($dR * $sR / 255, $dG * $sG / 255, $dB * $sB / 255)));
+        //(A + B) / 2
+        self::pixelHandler($topImage, $bottomImage, function($A, $B) {
+            return $A + $B / 2;
         });
     }
 
-    public static function layerDiference($dstImage, $srcImage, $dstSource, $srcSource)
+    public static function layerAdd($topImage, $bottomImage)
+    {
+        //min(255, (A + B))
+        self::pixelHandler($topImage, $bottomImage, function($A, $B) {
+            return min(255, ($A + $B));
+        });
+    }
+
+    public static function layerSubtract($topImage, $bottomImage)
+    {
+        //(A + B < 255) ? 0:(A + B - 255)
+        self::pixelHandler($topImage, $bottomImage, function($A, $B) {
+            return $A + $B < 255 ? 0 : $A + $B - 255;
+        });
+    }
+
+    public static function layerDiference($topImage, $bottomImage)
     {
         //差值
+        //abs(A - B)
+        self::pixelHandler($topImage, $bottomImage, function($A, $B) {
+            return abs($A - $B);
+        });
     } 
 
-    public static function layerExclusion($dstImage, $srcImage, $dstSource, $srcSource)
+    public static function layerNegation($topImage, $bottomImage)
     {
-        //排除
-    }
+        //255 - abs(255 - A - B)
+        self::pixelHandler($topImage, $bottomImage, function($A, $B) {
+            return abs(255 - $A - $B);
+        });
+    } 
 
-    public static function layerSoftLight($dstImage, $srcImage, $dstSource, $srcSource)
+    public static function layerScreen($topImage, $bottomImage)
     {
-        //柔光
-    }
-
-    public static function layerVividLight($dstImage, $srcImage, $dstSource, $srcSource)
-    {
-        //亮光
-    }
-
-    public static function layerLinearLight($dstImage, $srcImage, $dstSource, $srcSource)
-    {
-        //线性光
-        //TODO
-        self::pixelHandler($dstImage, $srcImage, function($point, $dR, $dG, $dB, $dA, $sR, $sG, $sB, $sA)
-             use ($dstImage, $srcImage){
-                 $dstImage->draw()->dot($point, new Color(array(
-                     ($r = $dR + 2 * $sR - 255) > 255 ? 255 : ($r < 0 ? 0 : $r),
-                     ($g = $dG + 2 * $sG - 255) > 255 ? 255 : ($g < 0 ? 0 : $g),
-                     ($b = $dB + 2 * $sB - 255) > 255 ? 255 : ($b < 0 ? 0 : $b)
-                 )));
+        //滤色
+        //255 - (((255 - A) * (255 - B)) >> 8))
+        self::pixelHandler($topImage, $bottomImage, function($A, $B) {
+            return 255 - ( ((255 - $A) * (255 - $B)) >> 8);
         });
     }
 
-    public static function layerPinLight($dstImage, $srcImage, $dstSource, $srcSource)
+    public static function layerExclusion($topImage, $bottomImage)
     {
-        //点光
+        //排除
+        //A + B - 2 * A * B / 255
+        self::pixelHandler($topImage, $bottomImage, function($A, $B) {
+            return $A + $B - 2 * $A * $B / 255;
+        });
     }
 
-    public static function layerHardMix($dstImage, $srcImage, $dstSource, $srcSource)
+    public static function layerOverlay($topImage, $bottomImage)
+    {
+        //叠加
+        //(B < 128) ? (2 * A * B / 255):(255 - 2 * (255 - A) * (255 - B) / 255)
+        self::pixelHandler($topImage, $bottomImage, function($A, $B) {
+            return ($B < 128) ? (2 * $A * $B / 255) : (255 - 2 * (255 - $A) * (255 - $B) / 255);
+        });
+    }
+
+    public static function layerSoftLight($topImage, $bottomImage)
+    {
+        //柔光
+        //(B < 128)?(2*((A>>1)+64))*((float)B/255):(255-(2*(255-((A>>1)+64))*(float)(255-B)/255))
+        self::pixelHandler($topImage, $bottomImage, function($A, $B) {
+            return $B < 128 ? 
+             (2 * (( $A >> 1) + 64)) * ($B / 255) : 
+             (255 - ( 2 * (255 - ( ($A >> 1) + 64 ) )  *  ( 255 - $B ) / 255 ));
+        });
+    }
+
+    public static function layerHardLight($topImage, $bottomImage)
+    {
+        //强光
+        //Overlay(B,A)
+        self::pixelHandler($topImage, $bottomImage, function($A, $B) {
+            return ($A < 128) ? (2 * $A * $B / 255) : (255 - 2 * (255 - $A) * (255 - $B) / 255);
+        });
+    }
+
+    public static function layerColorDodge($topImage, $bottomImage)
+    {
+        //颜色减淡
+        //(B == 255) ? B:min(255, ((A << 8 ) / (255 - B)))
+        self::pixelHandler($topImage, $bottomImage, function($A, $B) {
+            return $B == 255 ? $B : min(255, (($A << 8 ) / (255 - $B)));
+        });
+    }
+
+    public static function layerColorBurn($topImage, $bottomImage)
+    {
+        //颜色加深
+        //(B == 0) ? B:max(0, (255 - ((255 - A) << 8 ) / B)))
+        self::pixelHandler($topImage, $bottomImage, function($A, $B) {
+            return $B == 0 ? $B : max(0, (255 - ((255 - $A) << 8 ) / $B));
+        });
+    }
+
+    public static function layerLinearDodge($topImage, $bottomImage)
+    {
+        //线性减淡
+        self::layerAdd($topImage, $bottomImage);
+    }
+
+
+    public static function layerLinearBurn($topImage, $bottomImage)
+    {
+        //线性加深
+        self::layerSubtract($topImage, $bottomImage);
+    }
+
+    public static function layerLinearLight($topImage, $bottomImage)
+    {
+        //线性光
+        //(B < 128) ? LinearBurn(A,(2 * B)) : LinearDodge(A,(2 * (B - 128)))
+        
+        //min(255, (A + B))
+        //(A + B < 255) ? 0:(A + B - 255)
+        self::pixelHandler($topImage, $bottomImage, function($A, $B) {
+            return $B < 128 ? 
+                min(255, ($A + 2 * $B)) :
+                $A + (2 * ($B - 128)) < 255 ? 0 : $A + (2 * ($B - 128)) - 255
+            ;
+        });
+    }
+
+    public static function layerVividLight($topImage, $bottomImage)
+    {
+        //亮光
+        //B < 128 ? ColorBurn(A,(2 * B)) : ColorDodge(A,(2 * (B - 128)))
+        self::pixelHandler($topImage, $bottomImage, function($A, $B) {
+            return $B < 128 ? 
+            (
+                $B == 0 ? 2 * $B : max(0, (255 - ((255 - $A) << 8 ) / (2 * $B)))
+            ) :
+            (
+                (2 * ($B - 128)) == 255 ? (2 * ($B - 128)) : min(255, (($A << 8 ) / (255 - (2 * ($B - 128)) )))
+            ) ;
+        });
+    }
+
+    public static function layerPinLight($topImage, $bottomImage)
+    {
+        //点光
+        //(B < 128) ? Darken(A,(2 * B)) : Lighten(A,(2 * (B - 128))
+        self::pixelHandler($topImage, $bottomImage, function($A, $B) {
+            return $B < 128 ? 
+            (
+                2 * $B > $A ? $A : 2 * $B
+            ) :
+            (
+                (2 * ($B - 128)) > $A ? (2 * ($B - 128)) : $A
+            ) ;
+        });
+    }
+
+    public static function layerHardMix($topImage, $bottomImage)
     {
         //实色混合
+        //(VividLight(A,B) < 128) ? 0:255)
+        self::pixelHandler($topImage, $bottomImage, function($A, $B) {
+            return ($B < 128 ? 
+            (
+                $B == 0 ? 2 * $B : max(0, (255 - ((255 - $A) << 8 ) / (2 * $B)))
+            ) :
+            (
+                (2 * ($B - 128)) == 255 ? (2 * ($B - 128)) : min(255, (($A << 8 ) / (255 - (2 * ($B - 128)) )))
+            ))
+            < 128 ? 0 : 255 ;
+        });
+    }
+
+    public static function layerReflect($topImage, $bottomImage)
+    {
+        //(B == 255) ? B : min(255, (A * A / (255 - B)))
+        self::pixelHandler($topImage, $bottomImage, function($A, $B) {
+            return $B == 255 ? $B : min(255, ($A * $A / (255 - $B)));
+        });
+    }
+
+    public static function layerPhoenix($topImage, $bottomImage)
+    {
+        //min(A,B) - max(A,B) + 255
+        self::pixelHandler($topImage, $bottomImage, function($A, $B) {
+            return abs(min($A, $B) - max($A, $B) - 255);
+        });
     }
 
 }
